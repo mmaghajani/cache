dc20fbb320e10b363dad0ff9c36a26413d7a1ed7
mmaghajani <mma137421@gmail.com>
2016-03-23 20:39:17 +0430

    Adds controller

---
+++ controller.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity controller is port(
+  write_request : in std_logic ;
+  read_request: in std_logic ;
+  ram_ready: in std_logic
+  ) ;
+end controller;
+
+architecture behavioral of controller is
+  constant s0: integer := 0 ;
+  constant s0: integer := 0 ;
+  constant s0: integer := 0 ;
+  constant s0: integer := 0 ;
+  
+begin
+  
+end behavioral ;
