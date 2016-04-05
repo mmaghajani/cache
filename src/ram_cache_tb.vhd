84d083a253b2a328e433c9b20240ec26cf1a8c4d
mmaghajani <mma137421@gmail.com>
2016-04-05 18:56:48 +0430

    Makes big test bench for ram cache

--- src/ram_cache_tb.vhd
+++ src/ram_cache_tb.vhd
@@ -18,25 +18,44 @@ architecture test_bench of cache_ram_tb is
     );
     end component;
 
-    signal clk : STD_LOGIC := '0';
-    signal reset_n, read, write : STD_LOGIC;
-    signal addr: STD_LOGIC_VECTOR(9 downto 0);
-    signal wrdata,rddata : STD_LOGIC_VECTOR(31 downto 0);
-    signal hit : STD_LOGIC;
+    signal clk : std_logic := '0';
+    signal reset_n, read, write : std_logic;
+    signal addr: std_logic_vector(9 downto 0);
+    signal wrdata,rddata : std_logic_vector(31 downto 0);
+    signal hit : std_logic;
 
 begin
     mapping : ram_cache port map (clk , addr , reset_n, read, write , wrdata,
     rddata, hit);
 
-    addr <= STD_LOGIC_VECTOR(to_unsigned(0,10)),
-            STD_LOGIC_VECTOR(to_unsigned(13,10)) after 500 ns;
+    addr <= std_logic_vector(to_unsigned(52,10)),
+            std_logic_vector(to_unsigned(0,10)) after 320 ns,
+            std_logic_vector(to_unsigned(1,10)) after 620 ns,
+            std_logic_vector(to_unsigned(64,10)) after 920 ns,
+            std_logic_vector(to_unsigned(0,10)) after 1220 ns,
+            std_logic_vector(to_unsigned(1,10)) after 1520 ns,
+            std_logic_vector(to_unsigned(0,10)) after 1820 ns,
+            std_logic_vector(to_unsigned(64,10)) after 2120 ns,
+            std_logic_vector(to_unsigned(65,10)) after 2420 ns,
+            std_logic_vector(to_unsigned(52,10)) after 2720 ns;
+            
             
 
-    wrdata <= STD_LOGIC_VECTOR(to_unsigned(56,32)),
-              STD_LOGIC_VECTOR(to_unsigned(98,32)) after 500 ns;
-    write <= '0', '1' after 210 ns , '0' after 260 ns;
-    read <= '1', '0' after 70 ns;
-    reset_n <= '0';
+    wrdata <= std_logic_vector(to_unsigned(13,32)),
+              std_logic_vector(to_unsigned(52,32)) after 320 ns,
+              std_logic_vector(to_unsigned(12,32)) after 620 ns,
+              std_logic_vector(to_unsigned(4,32)) after 920 ns,
+              std_logic_vector(to_unsigned(25,32)) after 2420 ns,
+              std_logic_vector(to_unsigned(70,32)) after 2720 ns;
+              
+              
+    
+    write <= '1', '0' after 70 ns , '1' after 320 ns , '0' after 370 ns , '1' after 620 ns , '0' after 670 ns ,
+       '1' after 920 ns , '0' after 970 ns , '1' after 2420 ns , '0' after 2470 ns , '1' after 2720 ns , '0' after 2770 ns;
+    read <= '0', '1' after 1220 ns , '0' after 1270 ns , '1' after 1520 ns , '0' after 1570 ns , '1' after 1820 ns , 
+       '0' after 1870 ns , '1' after 2120 ns , '0' after 2170 ns ;
+    
+    reset_n <= '0' , '1' after 3020 ns ;
 
     CLOCK:
     clk <= '1' after 25 ns when clk = '0' else
